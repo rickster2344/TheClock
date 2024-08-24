7d526880e1546041613181bc853088efc794b965
Ricky Lam <rickster2344@gmail.com>
2024-08-24 00:00:07 -0400

    face detection causes light to blink

---
+++ eyefacedetect.py
@@ -0,0 +1,38 @@
+import cv2
+import pyfirmata as pyf
+
+board = pyf.Arduino('COM3')
+
+
+# from:https://www.youtube.com/watch?v=q9wvP6Li5Xo
+# haar cascades documentation: https://docs.opencv.org/4.0.0/d7/d8b/tutorial_py_face_detection.html 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
+# eye_cascade = cv2.CascadeClassifier('haarcascade_eye_tree_eyeglasses.xml')
+cap = cv2.VideoCapture(0)
+
+while cap.isOpened():
+    _, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y , w ,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0 , 0), 3)
+        roi_gray = gray[y:y+h, x:x+w]
+        
+        # roi_color = img[y:y+h, x:x+w]
+        # eyes = eye_cascade.detectMultiScale(roi_gray)
+        # for (ex, ey ,ew, eh) in eyes:
+        #     cv2.rectangle(roi_color, (ex,ey), (ex+ew, ey+eh), (0, 255, 0), 5)
+
+    # Display the output
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    if (len(faces)> 0):
+        board.digital[13].write(1)
+    else:
+        board.digital[13].write(0)
+
+
+cap.release()
